959a8d5df81bd4a03ff63de0b63277a4aac231b8
gaga1313 <gauravgaonkar13.13@gmail.com>
2025-04-14 23:42:33 -0400

    new update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import HeroImage from "../assets/portfolio/gaurav.jpeg";
 
 const About = () => {
   return (
@@ -13,17 +12,6 @@ const About = () => {
             About Me
           </p>
         </div>
-
-        <div className="md:hidden block ">
-          <img
-            src={HeroImage}
-            alt="my profile"
-            className="rounded-2xl mx-auto w-[95%]  md:hidden block "
-          />
-        </div>
-
-        <br />
-
         <p className="text-xl my-5">
         I'm Gaurav, a CS Master’s graduate from Brown University with a strong background in machine learning, computer vision, and large-scale model optimization. My research, published at venues like ICLR, focused on bridging the gap between human perception and state of the art computer vision models. With over two years of experience as an ML and Research Engineer at top tech companies, I’m passionate about building impactful AI systems and am actively seeking opportunities in applied ML, AI research, or engineering roles. Please feel free to reach out on LinkedIn or at <span className="font-extrabold">ggaonkar.work@gmail.com</span>
         </p>
